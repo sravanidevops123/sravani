d870a9d6ae4766693cb98d71a9063fd9dd3e30a8
devopscoursedata <102580717+devopscoursedata@users.noreply.github.com>
2022-07-23 20:30:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "awsinsta" {
-  ami             = "ami-068257025f72f470d "
+  ami             = "ami-068257025f72f470d"
   instance_type   = "t2.micro"
   security_groups = ["launch-wizard-1"]
   key_name        = "demo"
