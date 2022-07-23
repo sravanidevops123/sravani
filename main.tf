26111d6ea8bbf72bd4f11f36f0e4a8d30ac8ec97
devopscoursedata <102580717+devopscoursedata@users.noreply.github.com>
2022-07-23 21:43:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA2D6MKHV5MJ6EU6TG"
-  secret_key = "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"
+  access_key = "AKIA2D6MKHV5LV67JIPR"
+  secret_key = "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"
 }
 
 resource "aws_instance" "awsinsta" {
