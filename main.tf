c927049246f0bd6789f7843f13fec3d7b1607312
devopscoursedata <102580717+devopscoursedata@users.noreply.github.com>
2022-07-24 20:08:03 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region     = "ap-south-1"
+  
+}
+
+resource "aws_instance" "awsinsta" {
+  ami             = "ami-068257025f72f470d"
+  instance_type   = "t2.micro"
+  security_groups = ["launch-wizard-1"]
+  key_name        = "demo"
+  tags = {
+    Name = "terraform"
+  }
+
+provisioner "local-exec" {
+    when    = destroy
+    command = "echo 'Destroy-time provisioner'"
+  }
+  
+  provisioner "file" {
+    source      = "/target/vtdemo.war"
+    destination = "~/vtdemo.war"
+  }
+  
+   connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    #password = var.root_password
+    private_key = file("demo.pem")
+    host     = self.public_ip
+  }
+  
+  provisioner "remote-exec" {
+    script="downloadtomcat&java.sh"
+  }
+  
+  provisioner "remote-exec" {
+    script="start tomcat.sh"
+  }
+}
+
+output "my_publi_ip" {
+  value = aws_instance.awsinsta.public_ip
+}
