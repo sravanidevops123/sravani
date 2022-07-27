a710727cbb28cddec56716871ba356b1de158553
devopscoursedata <102580717+devopscoursedata@users.noreply.github.com>
2022-07-27 18:01:26 +0530

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -28,11 +28,22 @@ provisioner "local-exec" {
   }
   
   provisioner "remote-exec" {
-    script="downloadtomcatjava.sh"
+    inline= [
+      "sudo yum update -y && sudo yum install java-1.8.0-openjdk wget -y",
+      "sudo chown ec2-user:ec2-user /opt/",
+      "wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.41/bin/apache-tomcat-8.5.41.tar.gz &",
+      "sleep 15s",
+      "tar -xzvf apache-tomcat-8.5.41.tar.gz",
+      "mv apache-tomcat-8.5.41 /opt/tomcat8.5.41 "
+      ]
   }
   
   provisioner "remote-exec" {
-    script="starttomcat.sh"
+    inline= [
+      "cp ~/vtdemo.war /opt/tomcat8.5.41/webapps",
+     "nohup sh /opt/tomcat8.5.41/bin/startup.sh",
+    "ps -ef | grep java"
+      ]
   }
    tags = {
     Name = "terraform"
