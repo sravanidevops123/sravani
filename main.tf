cfca5da33040c874c13646e322dc7740b22202d3
sivareddyhub <110104308+sivareddyhub@users.noreply.github.com>
2022-07-31 10:44:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region     = "ap-northeast-3"
+  region     = "ap-south-1"
   
 }
 
 resource "aws_instance" "awsinsta" {
-  ami             = "ami-044921b7897a7e0da"
+  ami             = "ami-08df646e18b182346"
   instance_type   = "t2.micro"
   security_groups = ["launch-wizard-2"]
   key_name        = var.key_name
